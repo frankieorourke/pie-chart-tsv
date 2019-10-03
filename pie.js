280a9b90d00c93278f57ef971f28d220300bafc4
FRANCES O'ROURKE <francesorourke@FRANCESs-MacBook-Air.local>
2019-10-03 14:43:54 -0600

    fjdks

--- pie.js
+++ pie.js
@@ -1,56 +1,45 @@
-// define data
-console.log("hello");
+
+//https://codepen.io/thecraftycoderpdx/pen/jZyzKo
+//https://bl.ocks.org/mbostock/1346395
+//https://bl.ocks.org/cflavs/ff1c6005fd7edad32641
+
 /** 
 var dataset = [
     {label: "Assamese", count: 13},
     {label: "Bengali", count: 83},
     {label: "Bodo", count: 1.4},
     {label: "Dogri", count: 2.3},
-    {label: "Gujarati", count: 46},
-    {label: "Hindi", count: 300},
-    {label: "Kannada", count: 38},
-    {label: "Kashmiri", count: 5.5},
-    {label: "Konkani", count: 5},
-    {label: "Maithili", count: 20},
-    {label: "Malayalam", count: 33},
-    {label: "Manipuri", count: 1.5},
-    {label: "Marathi", count: 72},
-    {label: "Nepali", count: 2.9},
-    {label: "Oriya", count: 33},
-    {label: "Punjabi", count: 29},
-    {label: "Sanskrit", count: 0.01},
-    {label: "Santhali", count: 6.5},
-    {label: "Sindhi", count: 2.5},
-    {label: "Tamil", count: 61},
-    {label: "Telugu", count: 74},
-    {label: "Urdu", count: 52}
   ];
   */
 
 // chart dimensions **
-var width = 1200;
+var width = 1300;
 var height = 800;
 
 // a circle chart needs a radius **
-var radius = Math.min(width, height) / 2;
+var radius = height / 2.5;
 
 // legend dimensions --
 var legendRectSize = 25; // defines the size of the colored squares in legend
 var legendSpacing = 6; // defines spacing between squares
 
 // define color scale **
-//var color = d3.scale.category20b();
-var color = d3.scaleSequential().interpolator(d3.interpolateInferno).domain([1,100]);
+//var color = d3.scaleOrdinal(d3.schemeCategory20b);
+
+//var color = d3.scaleSequential().interpolator(d3.interpolateRgb(pie, pie));
 // more color scales: https://bl.ocks.org/pstuffa/3393ff2711a53975040077b7453781a9
 //var color = d3.scaleOrdinal(d3.schemeCategory20c)
-
-// ** this does d3.select('#chart') vs. d3.select('body') **
+//var color = d3.scaleOrdinal()
+    //.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+//var color = d3.scaleSequential().domain([1,100])
+    //.interpolator(d3.interpolateViridis);
+    // ** this does d3.select('#chart') vs. d3.select('body') **
 var svg = d3.select('body') // select element in the DOM with id 'chart'
   .append('svg') // append an svg element to the element we've selected
   .attr('width', width) // set the width of the svg element we just added
   .attr('height', height) // set the height of the svg element we just added
   .append('g') // append 'g' element to the svg element
-  .attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')'); // our reference is now to the 'g' element. centerting the 'g' element to the svg element
+  .attr('transform', 'translate(' + (width / 3) + ',' + (height / 2) + ')'); // our reference is now to the 'g' element. centerting the 'g' element to the svg element
 
 var arc = d3.arc() // ** 
   .innerRadius(0) // none for pie chart
@@ -61,6 +50,13 @@ var pie = d3.pie() // start and end angles of the segments
   //.sort(null); // by default, data sorts in descending value. this will mess with our animation so we set it to null
 
 // define tooltip
+
+var tooltip = d3.select("body")
+                .append("div")
+                .attr("class", "tooltip");
+
+
+                
 /** 
 var tooltip = d3.select('#pie') // select element in the DOM with id 'chart' (changed to pie)
   .append('div') // append a div element to the element we've selected                                    
@@ -87,23 +83,16 @@ tooltip.append('div') // add divs to the tooltip defined above
 //     </div>
 //   </div>
 // </div>
-
-// dataset.forEach(function(d) {
-//   d.count = +d.count; // calculate count as we iterate through the data
-//   d.enabled = true; // add enabled property to track which entries are checked
-// });
 */
 
 
 //from https://bl.ocks.org/mbostock/1346395
 
-d3.tsv("data.tsv").then(function(data) {
+d3.tsv("data_ordered.tsv").then(function(data) {
     data.forEach(function(d) {
         d.data_usage = +d.data_usage;
-        //console.log(d.data_usage);
-        //console.log(d.user);
     });
-    
+    var color = d3.scaleOrdinal().domain(data).range(d3.schemeRdYlGn[11]);
     ////////////////////////////////////////////////////////////////
     ///////////////APPLES N ORANGES INTERACTION STUFF ///////////////
     ////////////////////////////////////////////////////////////////
@@ -125,81 +114,60 @@ d3.tsv("data.tsv").then(function(data) {
     */
 
 
-    //from square example
-    
-var Tooltip = d3.select('body')
-     .append("div_template")
-     .style("opacity", 0)
-     .attr("class", "tooltip")
-     .style("background-color", "white")
-     .style("border", "solid")
-     .style("border-width", "2px")
-     .style("border-radius", "5px")
-     .style("padding", "5px");
+    var mouseover = function(d){
+        tooltip.style("display", "block")
+        .style("left", d3.event.pageX + "px")
+        .style("top", d3.event.pageY - 70 + "px")
+        .html(d.data.user + "\n" + d.data.data_usage);
+    }
+    var mousemove = function(d) {
+        tooltip
+        .style("left", d3.event.pageX + "px")
+        .style("top", d3.event.pageY - 70 + "px")
+        .style("display", "block")
+        .html(d.data.user + "\n" + d.data.data_usage); 
+    }
+    var  mouseleave = function(d){
+        tooltip.style("display", "none");
+    }
 
-var mouseover = function(d){
-    Tooltip.style("opacity", 1);
-    d3.select(this).style("stroke", "black")
-    .style("opacity", 1);
-}
-var mousemove = function(d) {
-    Tooltip.html("some text" + d.value)
-    .style("left", "200px")
-    .style("top", "200px");
-}
-var  mouseleave = function(d){
-    Tooltip.style("opacity", 0);
-    d3.select(this).style("stroke", "none")
-    .style("opacity", 0.8);
-}
-
-var path = svg.datum(data).selectAll("path")
-.data(pie)
-.enter()
-.append("path")
-//.attr("fill", function(d, i) { return color(i); })
-.attr('fill', function(d, i) {return color(d.value) }) 
-.attr("d", arc)
-.on("mouseover", mouseover)
-.on("mousemove", mousemove)
-.on("mouseleave", mouseleave);
-
-console.log("2");
-   
+    var path = svg.selectAll("path")
+        .data(pie(data))
+        .enter()
+        .append("path")
+        //.attr("fill", function(d, i) { return color(i); })
+        .attr("d", arc)
+        .attr('fill', function(d, i) {return color(i) }) 
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave);
 
 
+    var legend = svg.selectAll('.legend')
+    .data(pie(data))
+    .enter()
+    .append('g')
+    .attr('transform', function(d, i) {
+        var height = (legendRectSize + legendSpacing);
+        var offset =  height * color.domain().length / 2 + 100;
+        var horz = radius + 50;
+        var vert = i * height - offset;
+        return 'translate(' + horz + ',' + vert + ')';
+    });
+    legend.append('rect')
+    .attr('width', legendRectSize)
+    .attr('height', legendRectSize)                                   
+    .style('fill', function(d, i){return color(i) })
+    .style('stroke', color);
+
+    legend.append('text')
+    .attr('x', legendRectSize + legendSpacing)
+    .attr('y', legendRectSize - legendSpacing)
+    .text(function(d) {
+        console.log(d.data.user); 
+        return d.data.user; });
   });
-/**  
-// creating the chart {from india examle}
-var path = svg.selectAll('path') // select all path elements inside the svg. specifically the 'g' element. they don't exist yet but they will be created below
-  .data(pie(dataset)) //associate dataset wit he path elements we're about to create. must pass through the pie function. it magically knows how to extract values and bakes it into the pie
-  .enter() //creates placeholder nodes for each of the values
-  .append('path') // replace placeholders with path elements
-  .attr('d', arc) // define d attribute with arc function above
-  .attr('fill', function(d) { return color(d.data.label); }) // use color scale to define fill of each label in dataset
-  .each(function(d) { this._current - d; }); // creates a smooth animation for each track
-
-// mouse event handlers are attached to path so they need to come after its definition
-path.on('mouseover', function(d) {  // when mouse enters div      
- var total = d3.sum(dataset.map(function(d) { // calculate the total number of tickets in the dataset         
-  return (d.enabled) ? d.count : 0; // checking to see if the entry is enabled. if it isn't, we return 0 and cause other percentages to increase                                      
-  }));                                                      
- var percent = Math.round(1000 * d.data.count / total) / 10; // calculate percent
- tooltip.select('.label').html(d.data.label); // set current label           
- tooltip.select('.count').html('$' + d.data.count); // set current count            
- tooltip.select('.percent').html(percent + '%'); // set percent calculated above          
- tooltip.style('display', 'block'); // set display                     
-});                                                           
-
-path.on('mouseout', function() { // when mouse leaves div                        
-  tooltip.style('display', 'none'); // hide tooltip for that element
- });
-
-path.on('mousemove', function(d) { // when mouse moves                  
-  tooltip.style('top', (d3.event.layerY + 10) + 'px') // always 10px below the cursor
-    .style('left', (d3.event.layerX + 10) + 'px'); // always 10px to the right of the mouse
-  });
-
+/** 
 // define legend
 var legend = svg.selectAll('.legend') // selecting elements with class 'legend'
   .data(color.domain()) // refers to an array of labels from our dataset
